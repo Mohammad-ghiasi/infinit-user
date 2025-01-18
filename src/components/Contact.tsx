eb2c3c366e46a383a94f1dd723b804cea86e6472
Mohammad-ghiasi <mohammadghiasi005@gmail.com>
2025-01-18 19:22:26 +0330

    second-edition

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,7 +4,7 @@ export default function Contact() {
   return (
     <section className="shadow-md text-gray-500 py-16 text-center">
       <h2 className="text-3xl font-bold">Get in Touch</h2>
-      <p className="mt-4">We'd love to hear from you!</p>
+      <p className="mt-4">We love to hear from you!</p>
       <div className="mt-6">
         <Link href="/about">
           <button className="px-6 py-3 text-gray-600 bg-gray-300 font-semibold rounded-lg hover:bg-gray-200">
